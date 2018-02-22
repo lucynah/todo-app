2d5d5c5f2776194c71cdaeb87f9e52a7eab7a3f0
Lucyna H <lucyna.hernet@gmail.com>
2018-02-22 23:04:58 +0100

    added saving data in localStorage

--- scripts/components/app.jsx
+++ scripts/components/app.jsx
@@ -18,22 +18,44 @@ export default class App extends React.Component {
         this.onSubmit = this.onSubmit.bind(this);
         this.onDeleteClick = this.onDeleteClick.bind(this);
         this.onTodoChange = this.onTodoChange.bind(this);
+        this.onBeforeUnload = this.onBeforeUnload.bind(this);
     }
 
     render() {
         return (
             <div>
                 <AddTodo onAdd={this.onSubmit} />
-                <TodoList items={this.state.items} onTodoChange={this.onTodoChange} onDeleteClick = {this.onDeleteClick}/>
+                <TodoList items={this.state.items} onTodoChange={this.onTodoChange} onDeleteClick={this.onDeleteClick} />
             </div>
         )
     }
 
+    onBeforeUnload(e) {
+        e.preventDefault();
+
+        localStorage.setItem('items', JSON.stringify(this.state.items));
+    };
+
+    componentDidMount() {
+        console.log("component did mount");
+        let oldItems = JSON.parse(localStorage.getItem('items')) || [];
+        console.log(oldItems);
+        this.setState({
+            items: oldItems
+        });
+
+        window.addEventListener("beforeunload", this.onBeforeUnload);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("beforeunload", this.onBeforeUnload);
+    }
+
     onTodoChange(todo) {
         const indexOfTodo = this.state.items.indexOf(todo);
-        
+
         let newItems = this.state.items.slice().map(todo => {
-            return Object.assign({}, todo);  
+            return Object.assign({}, todo);
         });
 
         newItems[indexOfTodo].isDone = !newItems[indexOfTodo].isDone;
@@ -49,9 +71,9 @@ export default class App extends React.Component {
         let newItems = this.state.items.slice();
         newItems.splice(indexOfTodo, 1);
         this.setState(
-            { 
+            {
                 items: newItems
-             }
+            }
         )
     }
 
